bb39a93187a4453ea3021cb04adef84d7fcdc267
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-04 13:31:39 +0530

    UC3_Part_Full_Time_Emp_Wage (Branh)

--- Program.cs
+++ Program.cs
@@ -1,26 +1,28 @@
 using System;
 
-namespace UC2_Emp_Wage_Cal
+namespace UC3_Part_Full_Time_Emp_Wage
 {
     class Program
     {
         static void Main(string[] args)
         {
             int IS_Full_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_Full_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+                empHrs = 4;
+            else if (empCheck == IS_Full_TIME)
                 empHrs = 8;
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage is:" + empWage);
-            else
-                empHrs = 0;
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Emp is Absent"+empWage);
+            Console.WriteLine("Emp Wage is :" + empWage);
+            Console.ReadKey();
+            else if (empCheck ==0)
+                Console.WriteLine("Emp is Absent!");
             Console.ReadKey();
         }
     }
