1f03390f7af0e5979d83a58cd13a1c7d358f1feb
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-04 13:15:56 +0530

    UC1_Emp_Attendance Added (Branch)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-Cusing System;
+using System;
 
 namespace UC1_Emp_Attendance
 {
     class Program
     {
         static void Main(string[] args)
-       {
-                Console.WriteLine("Hello World");
+        {
+            int IS_Full_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_Full_TIME)
+                Console.WriteLine("Emp is Present");
+            else
+                Console.WriteLine("Emp is Absent");
+            Console.ReadKey();
         }
     }
 }
