84af0e845d03b98302428b48fabc002a138cb627
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-04 13:40:19 +0530

    UC4_SolvingUsing_Switch (Branch) Added

--- Program.cs
+++ Program.cs
@@ -1,29 +1,34 @@
 using System;
 
-namespace UC3_Part_Full_Time_Emp_Wage
+namespace UC4_SolvingUsing_Switch
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int IS_Full_TIME = 1;
-            int IS_PART_TIME = 2;
+            //Constants
+            const int IS_Full_TIME = 1;
+            const int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else if (empCheck == IS_Full_TIME)
-                empHrs = 8;
+            switch (empCheck)
+            {
+                case IS_Full_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage is :" + empWage);
-            Console.ReadKey();
-            else if (empCheck ==0)
-                Console.WriteLine("Emp is Absent!");
-            Console.ReadKey();
         }
     }
 }
