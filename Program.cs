271d4bb645571bf4bb5a9800668d4cd7047e6d51
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-04 13:23:00 +0530

    UC2_Emp_Wage_Cal Added

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
 using System;
 
-namespace UC1_Emp_Attendance
+namespace UC2_Emp_Wage_Cal
 {
     class Program
     {
         static void Main(string[] args)
         {
             int IS_Full_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_Full_TIME)
-                Console.WriteLine("Emp is Present");
+                empHrs = 8;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage is:" + empWage);
             else
-                Console.WriteLine("Emp is Absent");
+                empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Emp is Absent"+empWage);
             Console.ReadKey();
         }
     }
 }
+
